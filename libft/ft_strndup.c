6fc043321aff42d7d0da7e92dc699e6acee4d5c6
okolinko <virussania@gmail.com>
2018-11-13 19:10:20 +0200

    first commit

---
+++ libft/ft_strndup.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strndup.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akolinko <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/04/18 14:43:55 by akolinko          #+#    #+#             */
+/*   Updated: 2018/04/18 14:45:13 by akolinko         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strndup(const char *s1, size_t n)
+{
+	char *dst;
+
+	if (!(dst = (char*)malloc(sizeof(char) * (n + 1))))
+		return (NULL);
+	dst = ft_strncpy(dst, s1, n);
+	dst[n] = '\0';
+	return (dst);
+}
